641bc2c2121cdca601629aebd19ac0f31cbb1496
Melike Vurucu <melikechan@tuta.io>
2025-02-18 21:35:08 +0300

    blog: add some stuff

---
+++ src/app/husejinbg/page.js
@@ -0,0 +1,19 @@
+export default async function Home() {
+  return (
+    <main className="flex flex-col mx-8 my-4 min-h-screen space-y-4 animate-fadeIn">
+      <h1 className="text-4xl font-bold">A tribute to someone significant</h1>
+      <details>
+        <summary>Thank you.</summary>
+        <p>
+          I wish you have understood the feelings better, but I will accept you
+          as you are. However, be careful about your thoughts and actions, some
+          of them are very dangerous. I think you also owe me an apology :)
+        </p>
+        <p>
+          With the oblivons and heavens, you have been a very significant person
+          for me, Hüseyin Burak.
+        </p>
+      </details>
+    </main>
+  );
+}
